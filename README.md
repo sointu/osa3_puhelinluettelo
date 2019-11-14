474ead47e19e869bc8ecff976006a4000cce4d49
sointu <katja.valanne@gmail.com>
2019-11-14 13:01:10 +0200

    build commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Backend:
+https://safe-ocean-25676.herokuapp.com/api/persons
---
+++ build/precache-manifest.b241f01f46cf26f205e8362a27e2a7b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286c306a5d18e13a71f7ee4eb85b0ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfad26114b4b764c248d",
+    "url": "/static/css/main.56ea6c3e.chunk.css"
+  },
+  {
+    "revision": "55242d7a06cf79b6f38e",
+    "url": "/static/js/2.011bd06c.chunk.js"
+  },
+  {
+    "revision": "bfad26114b4b764c248d",
+    "url": "/static/js/main.19a2fa07.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
